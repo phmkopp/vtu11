3d0e5a644366c93df03b982531bac82a84c0df28
Philipp Kopp <philipp.kopp@tum.de>
2019-08-22 15:54:45 +0200

    Added catch2 header

---
+++ test/writer_test.cpp
@@ -0,0 +1,18 @@
+#include "catch.hpp"
+#include "vtu11.hpp"
+
+#include <array>
+#include <vector>
+
+namespace vtu11
+{
+
+TEST_CASE("Testtesttest")
+{
+    //REQUIRE_NOTHROW( C = evaluate2DCurve( t, x, y, knotVector ) );
+    //REQUIRE( 1 == 1 );
+    //CHECK( 1.0 == Approx( 1.0 ) );
+}
+
+} // namespace vtu11
+
