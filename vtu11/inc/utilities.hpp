a5362f95a74f3211a6c95cd78e84419319bd0d03
Philipp Kopp <philipp.kopp@tum.de>
2021-03-11 15:10:55 +0100

    Finalize refatoring

--- vtu11/inc/utilities.hpp
+++ vtu11/inc/utilities.hpp
@@ -10,7 +10,6 @@
 #ifndef VTU11_UTILITIES_HPP
 #define VTU11_UTILITIES_HPP
 
-
 namespace vtu11
 {
 
@@ -20,6 +19,7 @@ inline std::string dataTypeString( );
 inline std::string endianness( );
 
 #define VTU11_CHECK( expr, message ) if( !( expr ) ) throw std::runtime_error( message )
+
 } // namespace vtu11
 
 #include "impl/utilities_impl.hpp"
