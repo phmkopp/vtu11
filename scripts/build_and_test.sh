7c5348379f19b53dd81cb5b43451f54b1e718b96
philbucher <philipp.bucher@tum.de>
2021-03-18 16:23:24 +0100

    VTU11_ENABLE_TESTS no longer exists

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -2,7 +2,7 @@
 
 # to be executed from the root-dir
 
-cmake -H"." -DVTU11_ENABLE_TESTS=ON -DVTU11_STRICT_COMPILER=ON -B"build"
+cmake -H"." -DVTU11_STRICT_COMPILER=ON -B"build"
 cmake --build "build"  -- -j$(nproc)
 
 cd build # tests need to be executed in the same dir as the test_runner, otherwise path are not working correctly
