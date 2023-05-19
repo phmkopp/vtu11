7c39d2f39fcef98dda4410d24e015601a9bb1c4b
Philipp <philipp.bucher@tum.de>
2023-05-19 23:17:41 +0200

    add CopmuteOffsets

--- vtu11/vtu11.hpp
+++ vtu11/vtu11.hpp
@@ -32,25 +32,28 @@ struct Vtu11UnstructuredMesh
   size_t numberOfCells( ){ return types_.size( ); }
 };
 
+//! Helper to compute the offsets for geometries with deterministic number of points
+std::vector<VtkIndexType> CopmuteOffsets(const std::vector<VtkCellType>& Types);
+
 /*! Write modes (not case sensitive):
- * 
- *  - Ascii 
+ *
+ *  - Ascii
  *  - Base64Inline
  *  - Base64Appended
  *  - RawBinary
  *  - RawBinaryCompressed
- *  
+ *
  *  Comments:
  *  - RawCompressedBinary needs zlib to be present. If VTU11_ENABLE_ZLIB
  *    is not defined, the uncompressed version is used instead.
  *  - Compressing data takes more time than writing more data uncompressed
- *  - Ascii produces surprisingly small files, is nice to debug, but 
+ *  - Ascii produces surprisingly small files, is nice to debug, but
  *    is rather slow to read in Paraview. Archiving ascii .vtu files using
  *    a standard zip tool (for example) produces decently small file sizes.
  *  - Writing raw binary data breakes the xml standard. To still produce
  *    valid xml files you can use base64 encoding, at the cost of having
- *    about 30% times larger files.  
- *  - Both raw binary modes use appended format 
+ *    about 30% times larger files.
+ *  - Both raw binary modes use appended format
  */
 
 //! Writes single file
@@ -66,7 +69,7 @@ void writePVtu( const std::string& path,
                 const std::string& baseName,
                 const std::vector<DataSetInfo>& dataSetInfo,
                 size_t numberOfFiles );
-	
+
 //! Forwards path/baseName.vtu to the writeVtu function
 template<typename MeshGenerator>
 void writePartition( const std::string& path,
