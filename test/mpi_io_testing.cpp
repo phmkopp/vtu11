2f7b29eacc3b0eaceddf9cf4824281c3c41d8bad
Philipp <philipp.bucher@tum.de>
2023-05-23 00:27:09 +0200

    test for ascii and binary write

--- test/mpi_io_testing.cpp
+++ test/mpi_io_testing.cpp
@@ -14,7 +14,7 @@
 namespace vtu11
 {
 
-TEST_CASE( "mpi_io_test_1" )
+TEST_CASE( "mpi_io_test_binary" )
 {
     int argc = 0;
     char** argv = nullptr;
@@ -27,29 +27,44 @@ TEST_CASE( "mpi_io_test_1" )
 
     std::cout << "Hello from rank" << rank << " / " << size << std::endl;
 
-
     MPI_File file_handle;
-    MPI_File_open(MPI_COMM_WORLD, "mpi_io_test.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file_handle);
+    MPI_File_open(MPI_COMM_WORLD, "mpi_io_test_binary.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file_handle);
 
-    int buf[10];
-    for (int i=0; i<10; i++) buf[i] = 100 * rank + i;
+    MPI_Offset offset = static_cast<MPI_Offset>(rank * static_cast<int>(sizeof(int)));
+    MPI_File_write_at(file_handle, offset, &rank, 1, MPI_INT, MPI_STATUS_IGNORE);
 
-    /* set file offset for this calling process */
-    // MPI_Offset offset = (MPI_Offset)rank * 10 * sizeof(int);
-    // MPI_File_set_view(file_handle, offset, MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
-    // MPI_File_write_all(file_handle, &buf[0], 10, MPI_INT, MPI_STATUS_IGNORE);
-
-    // MPI_File_write(file_handle, &buf[0], 10, MPI_INT, MPI_STATUS_IGNORE);
-
-
-    MPI_File_write_at(file_handle, rank*sizeof(int), &rank, 1, MPI_INT, MPI_STATUS_IGNORE);
-
-    std::cout << "Hello 222 from rank" << rank << " / " << size << std::endl;
     MPI_File_close(&file_handle);
 
     MPI_Finalize();
+} // mpi_io_test_binary
 
+TEST_CASE( "mpi_io_test_ascii" )
+{
+    int argc = 0;
+    char** argv = nullptr;
+    MPI_Init(&argc, &argv);
 
-} // write_Pyramids3D_Test"
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    std::cout << "Hello from rank" << rank << " / " << size << std::endl;
+
+    MPI_File file_handle;
+    MPI_File_open(MPI_COMM_WORLD, "mpi_io_test_ascii.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file_handle);
+
+    const std::string my_data = std::to_string(rank) + " ";
+
+    int offset;
+    int my_size = my_data.size();
+    MPI_Scan(&my_size, &offset, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    MPI_File_write_at(file_handle, offset-my_size, my_data.data(), my_data.size(), MPI_CHAR, MPI_STATUS_IGNORE);
+
+    MPI_File_close(&file_handle);
+
+    MPI_Finalize();
+} // mpi_io_test_ascii
 
 } // namespace vtu11
